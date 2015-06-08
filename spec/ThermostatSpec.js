aaccfef3ee9ced210c38b1b643d840bb17cb2440
Merve Silk <merve.silk@gmail.com>
2015-06-08 17:31:50 +0100

    all default states are tested and passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,20 +2,27 @@ describe("Thermo", function() {
 
   var thermo;
 
+    beforeEach(function() {
+      thermo = new Thermo();
+    });
+
   describe("by default, ", function() {
     it("starts at 20 degrees", function() {
-      thermo = new Thermo();
       expect(thermo.currentTemperature).toEqual(20);
     });
 
     it("has a minumum temperature of 10 degrees", function() {
-      thermo = new Thermo();
       thermo.decreaseTemperature(11);
       expect(thermo.currentTemperature).toEqual(10);
     });
 
-    it("power saving mode is 'ON'", function() {
+    it("has power saving mode (PSM) 'ON'", function() {
       expect(thermo.powerSavingState).toBe(true);
+    });
+
+    it("has a maximum temperature of 25 degrees (due to PSM)", function() {
+      thermo.increaseTemperature(6);
+      expect(thermo.currentTemperature).toEqual(25);
     });
   });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,6 +2,7 @@ var Thermo = function() {
   this.currentTemperature = 20;
   this.minimumTemperature = 10;
   this.powerSavingState = true;
+  this.maximumTemperature = 25;
 };
 
 Thermo.prototype.decreaseTemperature = function(degreesToChangeBy) {
@@ -9,5 +10,13 @@ Thermo.prototype.decreaseTemperature = function(degreesToChangeBy) {
     this.currentTemperature = this.minimumTemperature;
   } else {
     this.currentTemperature -= degreesToChangeBy;
+  }
+};
+
+Thermo.prototype.increaseTemperature = function(degreesToChangeBy) {
+  if (this.currentTemperature + degreesToChangeBy > this.maximumTemperature) {
+    this.currentTemperature = this.maximumTemperature;
+  } else {
+    this.currentTemperature += degreesToChangeBy;
   }
 };
